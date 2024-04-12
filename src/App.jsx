272185a8878567ac5bebd6e789aca350eb56384f
Sakrid1s <bilinets.vitalik@gmail.com>
2024-04-12 19:13:43 +0300

    upd app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
-import './App.css';
+import { NavLink, Route, Routes } from 'react-router-dom';
+import clsx from 'clsx';
+import css from './App.module.css';
+import Home from './pages/Home';
+import Movies from './pages/Movies';
+import NotFound from './pages/NotFound';
+
+const buildLinkClass = ({ isActive }) => {
+  return clsx(css.link, isActive && css.active);
+};
 
 function App() {
-  return <></>;
+  return (
+    <div>
+      <nav>
+        <NavLink to="/" className={buildLinkClass}>
+          Home
+        </NavLink>
+        <NavLink to="/movies" className={buildLinkClass}>
+          Movies
+        </NavLink>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div>
+      <p>Sorry, nothing found</p>
+    </div>
+  );
+};
+
+export default NotFound;
