618acd28145c0eb689bb364fe8116c44b71ab3c5
Sakrid1s <bilinets.vitalik@gmail.com>
2024-04-23 20:35:35 +0300

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,7 +8,7 @@ const buildLinkClass = ({ isActive }) => {
 
 const Header = () => {
   return (
-    <div>
+    <header>
       <nav>
         <NavLink to="/" className={buildLinkClass}>
           Home
@@ -17,7 +17,7 @@ const Header = () => {
           Movies
         </NavLink>
       </nav>
-    </div>
+    </header>
   );
 };
 
